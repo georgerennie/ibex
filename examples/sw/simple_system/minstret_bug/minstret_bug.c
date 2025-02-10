65b3307cb46aded06615679ee5063b4db0ac4418
George Rennie <georgerennie@gmail.com>
2025-02-10 15:39:00 +0100

    add opentitan minstret bug reproducer

---
+++ examples/sw/simple_system/minstret_bug/minstret_bug.c
@@ -0,0 +1,11 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "simple_system_common.h"
+
+// int main(int argc, char **argv) {
+//   puthex(0x111);
+
+//   return 0;
+// }
